fd79b58a7b4e6b22b275e5b555a243dad62ee8d9
Onievaaaa <202302833@alu.comillas.edu>
2025-02-21 08:33:00 +0100

    V1

--- src/Lab_2_4_LR2.py
+++ src/Lab_2_4_LR2.py
@@ -46,7 +46,7 @@ class LinearRegressor:
         )  # Adding a column of ones for intercept
 
         if method == "least_squares":
-            self.fit_multiple(X_with_bias, y)
+            self.fit_multiple(X, y)
         elif method == "gradient_descent":
             self.fit_gradient_descent(X_with_bias, y, learning_rate, iterations)
 
@@ -66,9 +66,11 @@ class LinearRegressor:
         """
         # Replace this code with the code you did in the previous laboratory session
 
-        # Store the intercept and the coefficients of the model
-        self.intercept = None
-        self.coefficients = None
+        # Store the intercept and the coefficients of the modelx
+        x_nueva = np.c_[np.ones(X.shape[0]),X]
+        matriz = np.linalg.inv(x_nueva.T @ x_nueva) @ x_nueva.T @ y
+        self.intercept = matriz[0]
+        self.coefficients = matriz[1:]
 
     def fit_gradient_descent(self, X, y, learning_rate=0.01, iterations=1000):
         """
@@ -93,17 +95,18 @@ class LinearRegressor:
 
         # Implement gradient descent (TODO)
         for epoch in range(iterations):
-            predictions = None
+            predictions = self.intercept + np.dot(X[:,1:], self.coefficients)
             error = predictions - y
 
             # TODO: Write the gradient values and the updates for the paramenters
-            gradient = None
-            self.intercept -= None
-            self.coefficients -= None
+
+            gradient = (1 / len(X)) * X.T @ error
+            self.intercept -= learning_rate*gradient[0]
+            self.coefficients -= learning_rate*gradient[1:]
 
             # TODO: Calculate and print the loss every 10 epochs
             if epoch % 1000 == 0:
-                mse = None
+                mse = np.mean((error)**2)
                 print(f"Epoch {epoch}: MSE = {mse}")
 
     def predict(self, X):
@@ -125,8 +128,8 @@ class LinearRegressor:
 
         if self.coefficients is None or self.intercept is None:
             raise ValueError("Model is not yet fitted")
-
-        return None
+        if np.ndim():
+            return None
 
 
 def evaluate_regression(y_true, y_pred):
@@ -142,16 +145,16 @@ def evaluate_regression(y_true, y_pred):
     """
 
     # R^2 Score
-    # TODO
-    r_squared = None
+    rss = np.sum((y_pred - y_true)**2)
+    tss = np.sum((y_true - np.mean(y_true))**2)
+    r_squared = 1-(rss/tss)
 
     # Root Mean Squared Error
-    # TODO
-    rmse = None
+    mse = np.mean((y_pred - y_true)**2)
+    rmse = np.sqrt(mse)
 
     # Mean Absolute Error
-    # TODO
-    mae = None
+    mae = np.mean(np.abs(y_pred - y_true))
 
     return {"R2": r_squared, "RMSE": rmse, "MAE": mae}
 
@@ -171,20 +174,21 @@ def one_hot_encode(X, categorical_indices, drop_first=False):
     """
     X_transformed = X.copy()
     for index in sorted(categorical_indices, reverse=True):
-        # TODO: Extract the categorical column
-        categorical_column = None
+        categorical_column = X_transformed[:,index]
+        unique_values = list(set(categorical_column))
+        one_hot = [[0]*len(unique_values) for _ in range(len(categorical_column))]
 
-        # TODO: Find the unique categories (works with strings)
-        unique_values = None
-
-        # TODO: Create a one-hot encoded matrix (np.array) for the current categorical column
-        one_hot = None
-
-        # Optionally drop the first level of one-hot encoding
+        for i in range(len(categorical_column)):
+            for j in range(len(unique_values)):
+                if unique_values[j] == categorical_column[i]:
+                    one_hot[i][j] = 1
+        one_hot = np.array(one_hot)
         if drop_first:
             one_hot = one_hot[:, 1:]
 
-        # TODO: Delete the original categorical column from X_transformed and insert new one-hot encoded columns
-        X_transformed = None
+        izq = X_transformed[:, :index]
+        der = X_transformed[:, index+1:]
+        
+        X_transformed = np.concatenate([izq, one_hot, der], axis=1)
 
     return X_transformed
